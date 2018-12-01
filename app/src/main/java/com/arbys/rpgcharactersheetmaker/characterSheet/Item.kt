3153075cc708f9abaa269a33cb27d40b039bb558
coldmustard-alec <abg523@gmail.com>
2018-12-01 15:58:15 -0500

    addition of Item and Stats classes

---
+++ app/src/main/java/com/arbys/rpgcharactersheetmaker/characterSheet/Item.kt
@@ -0,0 +1,11 @@
+package com.arbys.rpgcharactersheetmaker.characterSheet
+
+class Item{
+
+    var itemName: String = "notset"
+    var itemType: String = "notset"
+    var cost: Int = 0
+    var itemAttributes = ArrayList<String>()
+    var notes = ArrayList<String>()
+
+}
--- app/src/main/java/com/arbys/rpgcharactersheetmaker/characterSheet/Stats.kt
+++ app/src/main/java/com/arbys/rpgcharactersheetmaker/characterSheet/Stats.kt
@@ -1,4 +1,11 @@
 package com.arbys.rpgcharactersheetmaker.characterSheet
 
 class Stats {
+
+    var strength: Int = 0
+    var dexteity : Int = 0
+    var constitution: Int = 0
+    var intelligence: Int = 0
+    var wisdom: Int = 0
+    var charisma: Int = 0
 }
