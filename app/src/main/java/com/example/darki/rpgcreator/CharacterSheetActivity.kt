10c504632cc2f37f47dd2fd4ff5a94d9604fd968
Alex Voytovich <darking271@gmail.com>
2018-12-04 11:59:40 -0500

    Some stuff

--- app/src/main/java/com/example/darki/rpgcreator/CharacterSheetActivity.kt
+++ app/src/main/java/com/example/darki/rpgcreator/CharacterSheetActivity.kt
@@ -9,7 +9,6 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.TextView
 import com.arbys.rpgcharactersheetmaker.characterSheet.CharacterSheet
 import com.example.darki.rpgcreator.fragments.StatsFragment
 import kotlinx.android.synthetic.main.activity_character_sheet.*
@@ -70,9 +69,6 @@ class CharacterSheetActivity :
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         when (item.itemId) {
             R.id.action_settings -> return true
             else -> return super.onOptionsItemSelected(item)
