100c3495098c6d2c15ec0c4733ceebf6b64caa71
coldmustard-alec <abg523@gmail.com>
2018-12-04 22:13:10 -0500

    Added method + helpers in "Item.kt" to return item attributes list

--- app/src/main/java/com/example/darki/rpgcreator/characterSheet/Item.kt
+++ app/src/main/java/com/example/darki/rpgcreator/characterSheet/Item.kt
@@ -10,7 +10,36 @@ class Item: Serializable {
     var itemAttributes = ArrayList<String>()
     var notes = ArrayList<String>()
 
+    fun getAttributesList(): ArrayList<String> {
+        val list = ArrayList<String>()
+        if (itemName != "")
+            list += "Item Name: $itemName"
+        if (itemType != "")
+            list += "Item Type: $itemType"
+        if (weight != 0)
+            list += "Weight: $weight"
+        if (cost != 0)
+            list += "Cost: $cost"
+        if (!itemAttributes.isEmpty())
+            list += "Item Attributes: " + list.addAll(itemAttributes)
+        if (!notes.isEmpty())
+            list += "Notes: " + list.addAll(notes)
+        return list
+    }
+
+    //basically a formatted toString():
+    private fun itemSplit(list: ArrayList<String>): String {
+
+        var retString = ""
+
+        for (i in list) {retString += (i + ", ")}
+
+        return retString
+    }
+
     override fun toString(): String {
-        return "$itemName: $itemType"
+        return "$itemName: $itemType: $weight: $cost: " +
+                itemSplit(itemAttributes) +
+                itemSplit(notes)
     }
 }
